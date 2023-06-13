4969250cd179e1e4d1a3ff920e38de95a92bf7f3
Andrey Blinov <bowltec@bk.ru>
2023-06-14 00:35:58 +0300

    Add Alerts for forgot buttons

--- HomeWork206/LoginViewController.swift
+++ HomeWork206/LoginViewController.swift
@@ -26,11 +26,31 @@ class LoginViewController: UIViewController {
         loginVC.welcomeLabelText = userName.text
        
     }
+    // MARK: - IBActions
+    
+    @IBAction func fogrotButtonsDidTapped(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            showAlert(withTitle: "Oops!", andMessage: "Your name is User")
+        default:
+            showAlert(withTitle: "Oops!", andMessage: "Your password is Password")
+        }
+    }
+    
     
     // MARK: - Private methods
     private func setupTextView(for textView: UITextField) {
         textView.layer.borderColor = UIColor.systemBlue.cgColor
         textView.layer.borderWidth = 1
+    }
+}
+
+extension LoginViewController {
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
 
